291c9d8253e0153475680c087c72c770b7442386
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-07 14:24:30 +0600

    Store data using variable and print

---
+++ Basic/variable.py
@@ -0,0 +1,15 @@
+# Tasrif Nur Himel
+
+# Variable 
+#           {}              .format()
+
+
+
+name = "Himel"
+age = 24
+
+
+print("My name is {} and my age is {}".format(name,age))
+
+
+print(f"My name is {name} and\nMy age is {age}")
