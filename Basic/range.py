baaab1d906f5cacc37675ac5ab35401a40fd388f
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-09 17:53:18 +0600

    Range function in python

---
+++ Basic/range.py
@@ -0,0 +1,15 @@
+# Range
+# Tasrif Nur Himel
+
+num = list(range(10))
+print(num)
+print(f"The value of 2 no index is : {num[2]}")
+
+
+# Start and End Point
+num = list(range(5,11)) #------> it actually means the range 5 to 10
+print(num)
+
+# Three perameter
+num = list(range(2,11,2)) # ------> 2,11 mean range 2 to 10 and last 2 mean distnace
+print(num)
