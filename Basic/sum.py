3f55d77d8f755293eaf83ef926c277e2af497fc1
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-09 15:47:57 +0600

    Sum of 1 to User Input

---
+++ Basic/sum.py
@@ -0,0 +1,24 @@
+# sum of 1 to user input
+# Tasrif Nur Himel
+
+'''def get_sum(z):
+    i = 1
+    sum = 0
+    while i<=z:
+        sum+=i
+        i+=1
+    print(f"The sum of 1 to {z} is {sum}")
+
+n = int(input("Enter a Number: "))
+get_sum(n)'''
+
+def get_sum(z):
+    i = 1
+    sum = 0
+    while i <= z:
+        sum += i
+        i += 1
+    print(f"The sum of 1 to {z} is: {sum}")
+
+n = int(input("Enter a Number: "))
+get_sum(n)
