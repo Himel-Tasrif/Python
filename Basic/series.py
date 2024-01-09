0f0ad9d0effca34fb88210f8a7f90394d6f17441
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-09 23:59:31 +0600

    Sum

--- Basic/series.py
+++ Basic/series.py
@@ -1,4 +1,9 @@
+## Tasrif Nur Himel 
+# Series  (1+2+3+4+..........+n)
 
-# Series
-n = int(input("Himel: "))
-print(n)
+n = int(input("Enter a number: "))
+
+sum = 0
+for i in range(1, n+1, 1):
+    sum += i
+print(f"Sum of 1 to {n} is: {sum}")
