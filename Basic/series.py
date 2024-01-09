76c7b8b6aa22f9dfe3a0e7eda621d24eca3223c1
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-10 00:04:33 +0600

    Series

--- Basic/series.py
+++ Basic/series.py
@@ -7,3 +7,14 @@ sum = 0
 for i in range(1, n+1, 1):
     sum += i
 print(f"Sum of 1 to {n} is: {sum}")
+
+'''
+Homework:
+2+4+6+8+.......+n 
+
+note one thing series depends on 3 things:
+1. range start with
+2. range end with
+3. distance between one from another
+
+'''
