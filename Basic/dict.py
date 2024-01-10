97c26fcb26354189715dcde1d71034f83b1a0895
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-10 23:30:29 +0600

    Dictionary

---
+++ Basic/dict.py
@@ -0,0 +1,14 @@
+#Dictionary
+# Tasrif Nur Himel
+id = {
+
+    '101': 'Tasrif Nur Himel',
+    '102': 'Waliul Islam Nohan',
+    '103': 'Joy Gopal'
+
+}
+
+print(id['101'])
+#OR
+print(id.get('102'))
+print(id.get('104', 'Not in dictionary'))
