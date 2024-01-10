a1d5a2cd07430910c221ce9c6af9575d62de9c02
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-10 23:34:36 +0600

    Touple

---
+++ Basic/tuple.py
@@ -0,0 +1,16 @@
+# Tuple
+# Tasrif Nur Himel
+
+sut = (
+    ('Tasrif Nur Himel','Busrat Jahan', 1078),
+    'Joy Gopal',
+    'James',
+    'Nohan',
+    'Nazafi',
+    885
+)
+
+print(sut)
+
+print(sut[3: ])
+print(sut[-1])
