0265e3e051bde231585ed2f813a0aa9e6ff29683
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-11 22:34:34 +0600

    Map & Filter

--- Basic/map.py
+++ Basic/map.py
@@ -1,5 +1,5 @@
-# Map
-
+# Map & Filter
+# Tasrif Nur Himel
 
 def squre(x):
     return x*x
@@ -9,4 +9,11 @@ num = [1,2,3,4,5]
 
 y = list(map(squre,num))
 
-print(y)
+print(f'Squre of all list values: {y}')
+
+
+# Filter
+
+result = list(filter(lambda i: i%2==00, num))
+
+print(f'Even number from {num} are: {result}')
