cd47df89bdc69e8f4d6165c77bc2731760f0ad30
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-11 22:18:08 +0600

    Lambda function

---
+++ Basic/lambda.py
@@ -0,0 +1,15 @@
+# Lambda Function(work at single line of code)
+
+# (a+b)^2 = a*a + 2*a*b + b*b
+
+'''
+def cal(a,b):
+    return a*a + 2*a*b + b*b
+
+print(f'The result is : {cal(1,2)}')'''
+
+#lambda parameter : argument
+
+x = (lambda a,b : a*a + 2*a*b + b*b)(1,2)
+
+print('The Result is: ',x)
