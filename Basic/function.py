9eb17ca0b0608669efb3619b422a92e4c2fbcf10
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-11 21:26:05 +0600

    Function

---
+++ Basic/function.py
@@ -0,0 +1,24 @@
+# Function
+# Tasrif Nur Himel
+
+def total(a,b):
+    sum = a+b
+    print(sum)
+
+def add(a,b,c):
+    sum = a+b+c
+    print(sum)
+
+total(1,2)
+total(2,2)
+total(3,2)
+add(1,2,3)
+
+# If you what to add  return type :
+
+def sub(a,b):
+    sub = a-b
+    return sub
+
+result = sub(100,5)
+print(result)
