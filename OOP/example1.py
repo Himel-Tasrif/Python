3b8906ad4a3b796df8030db84477592f65c89187
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-13 23:53:51 +0600

    Inheritance Example-1

--- OOP/example1.py
+++ OOP/example1.py
@@ -23,8 +23,19 @@ class rectangle(shape):
         area = self.v1 * self.v2
         print(f"Area of a rectangle: {area}")
 
+class circle(shape):
+    def __init__(self, r) -> None:
+        super().__init__(r, r)
+
+    def area(self):
+        area = 3.14 * self.v1 * self.v2
+        print(f'Area of a circle: {area}')
+
 t = triangle(10,20)
 t.area()
 
 r = rectangle(10,20)
 r.area()
+
+c = circle(10)
+c.area()
