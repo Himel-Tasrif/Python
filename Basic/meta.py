d6eef4ee57e67a35dc0e444768b1e9c61cfe90d6
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-01-17 21:12:52 +0600

    Meta Characters

--- Basic/meta.py
+++ Basic/meta.py
@@ -1,5 +1,42 @@
-import re
-pattern = r"colo.r"
+# Meta Character
+# Tasrif Nur Himel
 
-if re.match(pattern,"colour"):
+import re
+pattern1 = r"^colo..r$"
+
+if re.match(pattern1,"colouur"):
     print("Matched")
+else:
+    print("Unmatched")
+
+#(* -------> 0 or more)
+pattern2 = r"a*"
+
+if re.match(pattern2,"colouur"):
+    print("Matched")
+else:
+    print("Unmatched")
+
+#(+ -------> 1 or more)
+pattern3 = r"a+"
+
+if re.match(pattern3,"colouur"):
+    print("Matched")
+else:
+    print("Unmatched")
+
+#(? -------> 0 or 1)
+pattern4 = r"ice(-)?cream"
+
+if re.match(pattern4,"ice--cream"):
+    print("Matched")
+else:
+    print("Unmatched")
+
+#{} ------- range
+pattern5 = r"a{1,3}$"
+
+if re.match(pattern5,"aaa"):
+    print("Matched")
+else:
+    print("Unmatched")
