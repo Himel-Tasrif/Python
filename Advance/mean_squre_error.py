a0d9d5eac1341158dc89bb9b659f66205c3fb222
Himel-Tasrif <himel35-1078@diu.edu.bd>
2024-02-23 01:09:44 +0600

    Mean Squre Error

--- Advance/mean_squre_error.py
+++ Advance/mean_squre_error.py
@@ -1,8 +1,10 @@
 #Tasrif Nur Himel
+
+# Mean Squre Error:
 # 1/n sum(y-y')^2
 
 import numpy as np
-
+# if manual
 y_pred = np.array([1,1,0,0,1])
 y_true = np.array([0.30,0.7,1,0,0.5])
 
@@ -12,6 +14,9 @@ def _mean(y_pred, y_true):
         print(yt,yp)
 
         total_error += (yt - yp)*(yt - yp)
+
+    print(f'Total Squred Error: {total_error}')
+
     m = total_error / len(y_true)
 
     print(f'Mean Squre Error is: {m}')
@@ -19,3 +24,7 @@ def _mean(y_pred, y_true):
 
 
 _mean(y_pred, y_true)
+
+# If Using Numpy
+
+print(f'By Using Numpy---- Mean Squre Error: {np.mean(np.square(y_true - y_pred))}')
